5fc28f2624365043a831f573855d1edd926a1e3e
samuel <samuelincoln1@gmail.com>
2023-10-24 15:54:08 -0300

    att

--- Codigo/sistema/src/main/java/com/tamarana/sistema/repositories/VendaRep.java
+++ Codigo/sistema/src/main/java/com/tamarana/sistema/repositories/VendaRep.java
@@ -9,14 +9,5 @@ import com.tamarana.sistema.model.Venda;
 import com.tamarana.sistema.model.usuario.Usuario;
 
 public interface VendaRep extends JpaRepository<Venda, Integer> {
-    public List<Venda> findByUsuario (Usuario usuario);
-    
-    // @Query(value="SELECT * FROM usuario WHERE email = :email AND senha = :senha", nativeQuery = true)
-    // public Usuario Login(String email, String senha);
-
-
-  
-
-    
-
+    public List<Venda> findByUsuario (Usuario usuario); 
 }
