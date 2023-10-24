cd3e553b31c0eb3a6ae8a542d0cf21d28927daad
samuel <samuelincoln1@gmail.com>
2023-10-24 16:26:10 -0300

    att

--- Codigo/sistema/src/main/java/com/tamarana/sistema/repositories/VendaRep.java
+++ Codigo/sistema/src/main/java/com/tamarana/sistema/repositories/VendaRep.java
@@ -2,7 +2,6 @@ package com.tamarana.sistema.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.tamarana.sistema.model.Venda;
 
